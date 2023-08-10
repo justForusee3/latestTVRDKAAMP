8988bca4bd2258e45df724d309ffcac7c0ae668c
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-10 17:40:39 +0530

    Update latest.js

--- latest.js
+++ latest.js
@@ -26,41 +26,6 @@
 // Latest Code 
 
 
-// var url =
-//   "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
-// window.onload = function () {
-//   player = new AAMPMediaPlayer();
-// };
-
-// function loadAAMPVideo() {
-//   var video = document.getElementById("videoPlayer");
-//   video.src = url;
-//   video.play();
-//   video.style.display = "block";
-
-//   // Request full screen for the video element
-//   if (video.requestFullscreen) {
-//     video.requestFullscreen();
-//   } else if (video.mozRequestFullScreen) { // Firefox
-//     video.mozRequestFullScreen();
-//   } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-//     video.webkitRequestFullscreen();
-//   } else if (video.msRequestFullscreen) { // IE/Edge
-//     video.msRequestFullscreen();
-//   }
-// }
-
-// document.addEventListener("keydown", function (event) {
-//   if (event.keyCode === 13) {
-//     // Enter key
-//     loadAAMPVideo();
-//   }
-// });
-
-
-// Latest code 1.1
-
-
 var url =
   "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
 window.onload = function () {
@@ -85,14 +50,49 @@ function loadAAMPVideo() {
   }
 }
 
-// Trigger the function when the video container is clicked
-var videoContainer = document.getElementById("video-card-container");
-// videoContainer.addEventListener("click", loadAAMPVideo);
-
-videoContainer.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
     // Enter key
     loadAAMPVideo();
   }
 });
+
+
+// Latest code 1.1
+
+
+// var url =
+//   "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+// window.onload = function () {
+//   player = new AAMPMediaPlayer();
+// };
+
+// function loadAAMPVideo() {
+//   var video = document.getElementById("videoPlayer");
+//   video.src = url;
+//   video.play();
+//   // video.style.display = "block";
+
+//   // Request full screen for the video element
+//   if (video.requestFullscreen) {
+//     video.requestFullscreen();
+//   } else if (video.mozRequestFullScreen) { // Firefox
+//     video.mozRequestFullScreen();
+//   } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+//     video.webkitRequestFullscreen();
+//   } else if (video.msRequestFullscreen) { // IE/Edge
+//     video.msRequestFullscreen();
+//   }
+// }
+
+// // Trigger the function when the video container is clicked
+// var videoContainer = document.getElementById("video-card-container");
+// // videoContainer.addEventListener("click", loadAAMPVideo);
+
+// videoContainer.addEventListener("keydown", function (event) {
+//   if (event.keyCode === 13) {
+//     // Enter key
+//     loadAAMPVideo();
+//   }
+// });
 
