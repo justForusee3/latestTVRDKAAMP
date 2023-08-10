c0fdf070060c66661b2e38b4678e9110ea722a57
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-10 17:50:48 +0530

    Update latest.js

--- latest.js
+++ latest.js
@@ -26,36 +26,36 @@
 // Latest Code 
 
 
-var url =
-  "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
-window.onload = function () {
-  player = new AAMPMediaPlayer();
-};
+// var url =
+//   "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+// window.onload = function () {
+//   player = new AAMPMediaPlayer();
+// };
 
-function loadAAMPVideo() {
-  var video = document.getElementById("videoPlayer");
-  video.src = url;
-  video.play();
-  // video.style.display = "block";
+// function loadAAMPVideo() {
+//   var video = document.getElementById("videoPlayer");
+//   video.src = url;
+//   video.play();
+//   // video.style.display = "block";
 
-  // Request full screen for the video element
-  if (video.requestFullscreen) {
-    video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) { // Firefox
-    video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-    video.webkitRequestFullscreen();
-  } else if (video.msRequestFullscreen) { // IE/Edge
-    video.msRequestFullscreen();
-  }
-}
+//   // Request full screen for the video element
+//   if (video.requestFullscreen) {
+//     video.requestFullscreen();
+//   } else if (video.mozRequestFullScreen) { // Firefox
+//     video.mozRequestFullScreen();
+//   } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+//     video.webkitRequestFullscreen();
+//   } else if (video.msRequestFullscreen) { // IE/Edge
+//     video.msRequestFullscreen();
+//   }
+// }
 
-document.addEventListener("keydown", function (event) {
-  if (event.keyCode === 13) {
-    // Enter key
-    loadAAMPVideo();
-  }
-});
+// document.addEventListener("keydown", function (event) {
+//   if (event.keyCode === 13) {
+//     // Enter key
+//     loadAAMPVideo();
+//   }
+// });
 
 
 // Latest code 1.1
@@ -96,3 +96,33 @@ document.addEventListener("keydown", function (event) {
 //   }
 // });
 
+
+
+// Latest 1.2
+
+ var url = "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+
+        function loadAAMPVideo() {
+            var video = document.getElementById("videoPlayer");
+            video.src = url;
+            video.play();
+
+            // Request full screen for the video element
+            if (video.requestFullscreen) {
+                video.requestFullscreen();
+            } else if (video.mozRequestFullScreen) { // Firefox
+                video.mozRequestFullScreen();
+            } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+                video.webkitRequestFullscreen();
+            } else if (video.msRequestFullscreen) { // IE/Edge
+                video.msRequestFullscreen();
+            }
+        }
+
+        document.addEventListener("keydown", function (event) {
+            if (event.keyCode === 13) {
+                // Enter key
+                event.preventDefault(); // Prevent default behavior of the Enter key
+                loadAAMPVideo();
+            }
+        });
