7714797436932df280031588cca0ea7761267ae1
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-10 17:32:56 +0530

    Update latest.js

--- latest.js
+++ latest.js
@@ -26,6 +26,41 @@
 // Latest Code 
 
 
+// var url =
+//   "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+// window.onload = function () {
+//   player = new AAMPMediaPlayer();
+// };
+
+// function loadAAMPVideo() {
+//   var video = document.getElementById("videoPlayer");
+//   video.src = url;
+//   video.play();
+//   video.style.display = "block";
+
+//   // Request full screen for the video element
+//   if (video.requestFullscreen) {
+//     video.requestFullscreen();
+//   } else if (video.mozRequestFullScreen) { // Firefox
+//     video.mozRequestFullScreen();
+//   } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+//     video.webkitRequestFullscreen();
+//   } else if (video.msRequestFullscreen) { // IE/Edge
+//     video.msRequestFullscreen();
+//   }
+// }
+
+// document.addEventListener("keydown", function (event) {
+//   if (event.keyCode === 13) {
+//     // Enter key
+//     loadAAMPVideo();
+//   }
+// });
+
+
+// Latest code 1.1
+
+
 var url =
   "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
 window.onload = function () {
@@ -50,9 +85,7 @@ function loadAAMPVideo() {
   }
 }
 
-document.addEventListener("keydown", function (event) {
-  if (event.keyCode === 13) {
-    // Enter key
-    loadAAMPVideo();
-  }
-});
+// Trigger the function when the video container is clicked
+var videoContainer = document.getElementById("videoContainer");
+videoContainer.addEventListener("click", loadAAMPVideo);
+
