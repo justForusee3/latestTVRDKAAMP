1d969ba0bea48d6cb3a58dd9a50e99e55de97cca
justForusee3 <125953355+justForusee3@users.noreply.github.com>
2023-08-10 17:28:30 +0530

    Update latest.js

--- latest.js
+++ latest.js
@@ -1,19 +1,58 @@
+// var url =
+// "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+// window.onload = function () {
+// player = new AAMPMediaPlayer();
+// };
+
+// function loadAAMPVideo() {
+// var video = document.getElementById("videoPlayer");
+// video.src = url;
+// video.play();
+// video.style.display = "block";
+// }
+
+// document.addEventListener("keydown", function (event) {
+// if (event.keyCode === 13) {
+//   // Enter key
+//   loadAAMPVideo();
+// }
+// });
+
+
+
+
+
+
+// Latest Code 
+
+
 var url =
-"aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
+  "aamps://dash.akamaized.net/dash264/TestCasesMCA/dolby/3/1/ChID_voices_20_128_ddp.mpd";
 window.onload = function () {
-player = new AAMPMediaPlayer();
+  player = new AAMPMediaPlayer();
 };
 
 function loadAAMPVideo() {
-var video = document.getElementById("videoPlayer");
-video.src = url;
-video.play();
-video.style.display = "block";
+  var video = document.getElementById("videoPlayer");
+  video.src = url;
+  video.play();
+  video.style.display = "block";
+
+  // Request full screen for the video element
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) { // Firefox
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) { // IE/Edge
+    video.msRequestFullscreen();
+  }
 }
 
 document.addEventListener("keydown", function (event) {
-if (event.keyCode === 13) {
-  // Enter key
-  loadAAMPVideo();
-}
+  if (event.keyCode === 13) {
+    // Enter key
+    loadAAMPVideo();
+  }
 });
